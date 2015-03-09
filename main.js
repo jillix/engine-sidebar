cae03023c00d649c9aa5b940a457dfddf394f76e
Ionică Bizău <bizauionica@yahoo.com>
2015-03-09 17:36:10 +0200

    Initial

---
+++ main.js
@@ -0,0 +1,29 @@
+// Dependencies
+var $ = require("/libs/jquery");
+require("./sidebar")
+
+/**
+ * init
+ *
+ * @name init
+ * @function
+ * @return {undefined}
+ */
+exports.init = function () {
+
+    var conf = this._config;
+    conf.selector = conf.selector || ".sidebar[data-side]";
+
+    // Initialize the sidebars
+    $(conf.selector).each(function () {
+        var $this = $(this);
+        $this.sidebar({ side: $this.attr("data-side") });
+    });
+};
+
+["open", "close", "toggle"].forEach(function (c) {
+    exports[c] = function (_, data) {
+        $(data.sel).trigger("sidebar:" + c);
+    };
+});
+
